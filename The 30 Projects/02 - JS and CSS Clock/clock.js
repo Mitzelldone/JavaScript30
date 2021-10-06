2e1d0f47f9c9fca002a920324f981bb81fe91841
DoneCode96 <mitzelldone.mvn@gmail.com>
2021-10-06 17:25:56 +0800

    Update clock.js

--- The 30 Projects/02 - JS and CSS Clock/clock.js
+++ The 30 Projects/02 - JS and CSS Clock/clock.js
@@ -4,30 +4,28 @@ const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
-    const now = new Date();
+  const now = new Date();
 
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // Apply rotation to the clock hands corresponding with current time value
 
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     
-    // remove glitch.
-    if (secondsDegrees === 90) {
-      secondHand.style.transition = 'all 0s';
-    }else secondHand.style.transition = 'all 0.05s';
+  // remove glitch.
+  if (secondsDegrees === 90) {
+    secondHand.style.transition = 'all 0s';
+  }else secondHand.style.transition = 'all 0.05s';
 
-    if (minsDegrees === 90) { minHand.style.transition = 'all 0s';
-    }else minHand.style.transition = 'all 0.1s';
+  if (minsDegrees === 90) { minHand.style.transition = 'all 0s';
+  }else minHand.style.transition = 'all 0.1s';
 
-  }
-
-  setInterval(setDate, 1000);
-
-  setDate();
+}
+//run every second
+setInterval(setDate, 1000);
